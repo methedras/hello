d39b49a3cda2375ff927352e45a0a59e49891e71
Peter Fryscak <peterincali@gmail.com>
2015-01-24 08:35:59 -0800

    First
    First try to link and commit

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"windows_amd64\github.com\methedras\stringutil"
+)
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
 }
