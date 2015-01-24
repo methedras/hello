fb5b57275bff682edd10cd6295d86f35ad931e4d
Peter Fryscak <peterincali@gmail.com>
2015-01-24 10:09:39 -0800

    Read dict.txt
    Simple.go now reads and displays contents of dict.txt.

--- simple.go
+++ simple.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import ( 
+  "fmt" 
+  "io"
+  "os" 
+  "bufio"
+) 
 
 func main() {
-	fmt.Println("Hello, playground")
+	fmt.Println("Hello, Linen Project")
+	fin, err := os.Open("dict.txt")
+	if (err==nil) {
+		fmt.Println("File exists!")
+		r := bufio.NewReader(fin) 
+        for line, _, err := r.ReadLine(); err!=io.EOF; line, _, err = r.ReadLine() { 
+            fmt.Printf("Lines: %v (error %v)\n", string(line), err)  
+			}
+	} else {
+		fmt.Printf("Error: the file to be read is not found")
+	}
 }
